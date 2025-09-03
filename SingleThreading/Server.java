d1441a533a6b51c76485e8f34526f8d754606340
Tanushka12 <kashyaptanushka14@gmail.com>
2025-09-03 19:45:19 +0530

    Add Client and Server classes for single-threaded communication

---
+++ SingleThreading/Server.java
@@ -0,0 +1,47 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+public class Server{
+
+    public void run() throws IOException{
+        int port = 8010;
+        ServerSocket socket = null;
+        try {
+            socket = new ServerSocket(port);
+            socket.setSoTimeout(10000);
+            while (true) {
+                try {
+                    System.out.println("Server is listening on port " + port);
+                    Socket acceptedConnection = socket.accept();
+                    System.out.println("Connection accepted from " + acceptedConnection.getRemoteSocketAddress());
+                    PrintWriter toClient = new PrintWriter(acceptedConnection.getOutputStream());
+                    BufferedReader fromClient = new BufferedReader(new InputStreamReader(acceptedConnection.getInputStream()));
+                    toClient.println("Hello from the Server!");
+                    toClient.close();
+                    fromClient.close();
+                    acceptedConnection.close();
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } finally {
+            if (socket != null && !socket.isClosed()) {
+                socket.close();
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Server server = new Server();
+        try {
+            server.run();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
